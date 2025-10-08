115d0a3454112f9f8ece052914e5227a9b6701de
Philipp Craine <125037167+philippzhuravlev@users.noreply.github.com>
2025-10-09 01:43:02 +0200

    Update .eslintrc.js

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -11,13 +11,22 @@ module.exports = {
     "google",
   ],
   rules: {
-    "no-restricted-globals": ["error", "name", "length"],
-    "prefer-arrow-callback": "error",
-    "quotes": ["error", "double", {"allowTemplateLiterals": true}],
+    'no-restricted-globals': ['error', 'name', 'length'],
+    'max-len': 'off',
+    'prefer-arrow-callback': 'error',
+    'quotes': 'off', // allow both single and double quotes
+    'linebreak-style': 'off', // allow both CRLF and LF
+    'object-curly-spacing': 'off', // allow spacing in object literals
+    'comma-dangle': 'off', // allow trailing commas or not
+    'indent': 'off', // dont enforce strict indentation
+    'no-trailing-spaces': 'off', // allow trailing spaces
+    'eol-last': 'off', // dont require newline at end of file
+    'valid-jsdoc': 'off', // dont enforce JSDoc format
+    'arrow-parens': 'off', // dont require parens around arrow function args
   },
   overrides: [
     {
-      files: ["**/*.spec.*"],
+      files: ['**/*.spec.*'],
       env: {
         mocha: true,
       },
