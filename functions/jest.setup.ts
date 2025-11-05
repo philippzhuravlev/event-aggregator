434079a9882a2d51e2c964fd6786259a420bb81f
philippzhuravlev <philippzhuravlev@gmail.com>
2025-11-05 01:06:25 +0100

    delete old /functions content

--- functions/jest.setup.ts
+++
@@ -1,7 +0,0 @@
-// Load environment variables from .env for testing
-import dotenv from 'dotenv';
-
-// Load .env file relative to this file's directory
-dotenv.config({ path: './.env' });
-
-console.log('Jest setup: Loaded environment variables from .env');
